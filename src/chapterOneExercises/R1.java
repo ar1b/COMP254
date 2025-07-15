52bde2d78f40b012c5653c4322dd1857f8a6038e
ar1b <arib.kamal99@gmail.com>
2025-07-15 16:43:55 -0400

    added R-1.1

---
+++ src/chapterOneExercises/R1.java
@@ -0,0 +1,11 @@
+package chapterOneExercises;
+import java.util.Scanner;
+
+public class R1 {
+    public static void inputAllBaseTypes(){
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+    }
+}
